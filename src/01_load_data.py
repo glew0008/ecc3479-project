41ac2e3abb1163baae13432fb938b124a849f070
glew0008 <glew0008@student.monash.edu>
2026-03-29 13:07:02 +1100

    Load raw data 2021

--- src/01_load_data.py
+++ src/01_load_data.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-df = pd.read_excel("data/raw/2021 census data.xlsx")
-print(df.head())
+df = pd.read_excel(
+    "data/raw/2021 census data.xlsx",
+    header=None,
+    dtype=str
+)
+
+# Show first 60 rows and first 20 columns
+print(df.iloc[:60, :20])
